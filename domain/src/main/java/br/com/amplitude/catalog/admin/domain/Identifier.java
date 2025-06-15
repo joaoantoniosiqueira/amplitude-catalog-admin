9928a03fbd7c33ed6cb04885b6fd29cec5034ebb
João Antônio Ferreira Siqueira <joao.calvet.siqueira@gmail.com>
2025-06-14 22:09:47 -0300

    Create the DDD classes and making the Category class an aggregate root

---
+++ domain/src/main/java/br/com/amplitude/catalog/admin/domain/Identifier.java
@@ -0,0 +1,4 @@
+package br.com.amplitude.catalog.admin.domain;
+
+public abstract class Identifier extends ValueObject {
+}
--- domain/src/main/java/br/com/amplitude/catalog/admin/domain/catgory/Category.java
+++ domain/src/main/java/br/com/amplitude/catalog/admin/domain/catgory/Category.java
@@ -1,11 +1,11 @@
 package br.com.amplitude.catalog.admin.domain.catgory;
 
+import br.com.amplitude.catalog.admin.domain.AggregateRoot;
+
 import java.time.Instant;
-import java.util.UUID;
 
-public class Category {
+public class Category extends AggregateRoot<CategoryID> {
 
-    private String id;
     private String name;
     private String description;
     private boolean active;
@@ -13,29 +13,29 @@ public class Category {
     private Instant updatedAt;
     private Instant deletedAt;
 
-    private Category(final String id,
-                     final String name,
-                     final String description,
-                     final boolean active,
-                     final Instant createdAt,
-                     final Instant updatedAt,
-                     final Instant deletedAt) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.active = active;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.deletedAt = deletedAt;
+    private Category(final CategoryID anId,
+                     final String aName,
+                     final String aDescription,
+                     final boolean isActive,
+                     final Instant aCreatedAt,
+                     final Instant anUpdatedAt,
+                     final Instant aDeletedAt) {
+        super(anId);
+        this.name = aName;
+        this.description = aDescription;
+        this.active = isActive;
+        this.createdAt = aCreatedAt;
+        this.updatedAt = anUpdatedAt;
+        this.deletedAt = aDeletedAt;
     }
 
     public static Category newCategory(final String aName, final String aDescription, final boolean isActive) {
-        final var id = UUID.randomUUID().toString();
+        final var id = CategoryID.unique();
         final var now = Instant.now();
         return new Category(id, aName, aDescription, isActive, now, now, null);
     }
 
-    public String getId() {
+    public CategoryID getId() {
         return id;
     }
 
