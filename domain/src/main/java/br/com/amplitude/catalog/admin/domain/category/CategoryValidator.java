13cd2598c21e26428d00cc1dc3260680afe74909
João Antônio Siqueira <joao.calvet.siqueira@gmail.com>
2025-06-17 17:14:47 -0300

    Implementing more category validations

--- domain/src/main/java/br/com/amplitude/catalog/admin/domain/category/CategoryValidator.java
+++ domain/src/main/java/br/com/amplitude/catalog/admin/domain/category/CategoryValidator.java
@@ -15,8 +15,24 @@ public class CategoryValidator extends Validator {
 
     @Override
     public void validate() {
-        if (category.getName() == null || category.getName().isBlank()) {
+        checkNameConstraints();
+    }
+
+    private void checkNameConstraints() {
+        final var name = category.getName();
+        if (name == null) {
             validationHandler().append(new Error("'name' should not be null"));
+            return;
+        }
+
+        if (name.trim().isEmpty()) {
+            validationHandler().append(new Error("'name' should not be empty"));
+            return;
+        }
+
+        int length = name.trim().length();
+        if (length < 3 || length > 255) {
+            validationHandler().append(new Error("'name' must be between 3 and 255 characters"));
         }
     }
 }
--- domain/src/test/java/br/com/amplitude/catalog/admin/domain/category/CategoryTest.java
+++ domain/src/test/java/br/com/amplitude/catalog/admin/domain/category/CategoryTest.java
@@ -41,4 +41,95 @@ public class CategoryTest {
         Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
         Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
     }
+
+    @Test
+    public void shouldReturnExceptionWhenGivenEmptyNameParam() {
+        final var expectedName = "    ";
+        final var expectedDescription = "The category with the best movies";
+        final var expectedIsActive = true;
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void shouldReturnExceptionWhenGivenLengthLessThan3NameParam() {
+        final var expectedName = "Mo";
+        final var expectedDescription = "The category with the best movies";
+        final var expectedIsActive = true;
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void shouldReturnExceptionWhenGivenLengthGreaterThan255NameParam() {
+        final var expectedName = "Movies".repeat(100);
+        final var expectedDescription = "The category with the best movies";
+        final var expectedIsActive = true;
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void shouldReturnNewCategoryWhenGivenEmptyDescriptionParam() {
+        final var expectedName = "Movies";
+        final var expectedDescription = "   ";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertDoesNotThrow(() -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+
+    @Test
+    public void shouldReturnNewCategoryWhenGivenFalseActiveParam() {
+        final var expectedName = "Movies";
+        final var expectedDescription = "The category with the best movies";
+        final var expectedIsActive = false;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertDoesNotThrow(() -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNotNull(actualCategory.getDeletedAt());
+    }
 }
